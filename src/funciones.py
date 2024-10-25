6b37bd0de9a96b7728cd95b8d15e28d2e814c943
jgilsu11 <jaimegilnero@gmail.com>
2024-10-25 18:31:52 +0200

    Subida de extraccón casi lista

--- src/funciones.py
+++ src/funciones.py
@@ -1,11 +1,14 @@
 
+import pandas as pd
 
-
-def sacar_tabla(tabla_indiv):
+def sacar_tabla(tabla_indiv, mercado, producto, variante):
     tr_tabla=tabla_indiv.findAll("tr")
     th_tabla = tabla_indiv.findAll("th")
     td_tabla=tabla_indiv.findAll("td")
     df_cuerpo=pd.DataFrame([r.findAll('td') for r in tr_tabla][1:]).map(lambda x: x.text)
     dicc_encabezado=dict(enumerate(pd.DataFrame(th_tabla).T.values[0]))  #enumerate porque coincide el 0,1 y 2 con los titulos de las columnas
     df_tabla= df_cuerpo.rename(dicc_encabezado, axis=1)
+    df_tabla["supermercado"]= mercado
+    df_tabla["producto"]= producto
+    df_tabla["variante"]= variante
     return df_tabla
