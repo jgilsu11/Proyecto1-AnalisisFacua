6c06865b5e6c5a63bb46f94e32428af73e252622
jgilsu11 <jaimegilnero@gmail.com>
2024-10-26 14:28:32 +0200

    Subida con tablas y base de datos creada (lo proximo es hacer queries)

--- src/funciones.py
+++ src/funciones.py
@@ -12,3 +12,24 @@ def sacar_tabla(tabla_indiv, mercado, producto, variante):
     df_tabla["producto"]= producto
     df_tabla["variante"]= variante
     return df_tabla
+
+
+def crear_df_id (df, col):
+    df_nuevo= pd.DataFrame(df[col])
+    df_nuevo[f"id_{col}"]= pd.RangeIndex(start=1,stop = len(df_nuevo)+1,step=1)
+    return df_nuevo
+
+
+
+def añadir_id_mapeado(Df_diccionario, Columnadicc_nombre, Columnadicc_id, Columna_a_mapear, Nombre_nueva_col_id, Df_donde_mapear):
+    diccionario_prod= {}
+    datos = Df_diccionario.groupby(Columnadicc_nombre)[Columnadicc_id].first()
+    datos_prod = list(datos.index)
+    lista_prod = list(datos.values)
+    for i in range(0, len(datos_prod)):
+        diccionario_prod[datos_prod[i]] = lista_prod[i]
+
+    Df_donde_mapear[Nombre_nueva_col_id] = Df_donde_mapear[Columna_a_mapear].map(diccionario_prod)
+
+    Df_donde_mapear.drop(columns=Columna_a_mapear, inplace=True)
+    return Df_donde_mapear
